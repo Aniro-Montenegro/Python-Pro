7aaf3cdf716705100529be3f82d9719af918b417
79Aniro <capablanca1@bol.com.br>
2021-12-31 12:02:18 -0300

    Tipos float e complexos

---
+++ Tipos Básicos/tipo_float.py
@@ -0,0 +1,63 @@
+f = 1.56
+print(f)
+
+t = type(f)
+
+print(t)
+
+p = 0.25
+print("0.25 = " + str(0.25))
+
+p = .25
+print(".25 = " + str(0.25))
+
+n1 = .5
+n2 = .2
+
+print('.5 + .2 = ' + str(n1 + n2))
+print('.5 * .2 = ' + str(n1 * n2))
+print('.5 / .2 = ' + str(n1 / n2))
+print('.5 // .2 = ' + str(n1 // n2))
+print('.5 ** .2 = ' + str(n1 ** n2))
+print('.5 % .2 = ' + str(n1 % n2))
+
+# Transformar flot para inteiro
+
+pf = 1.0
+t = type(pf)
+
+print("1.0 = " + str(t))
+
+pi = int(pf)
+
+t = type(pi)
+
+print("int(pf) = " + str(t))
+
+pi = 5
+t = type(pi)
+
+print("5 = " + str(t))
+
+pf = float(pi)
+
+t = type(pf)
+
+print("float(pi) = " + str(t))
+
+# Truncagem
+
+k = 1.5
+
+n = int(k)
+
+print("Truncagem 1.5 para int =" + str(n))
+
+" Numero complexo"
+
+c = 1j
+
+print(type(c))
+
+print(1j ** 2)
+print(1j ** 4)
