7e36af5e0f676c6f50e0c1f1151cc67055a0b7be
79Aniro <capablanca1@bol.com.br>
2021-12-31 11:39:35 -0300

    Tipos inteiros

---
+++ Tipos Básicos/tipo_inteiro.py
@@ -0,0 +1,60 @@
+x = 123456
+print(x)
+t = type(x)
+print(t)
+
+# Operações
+
+# Soma
+
+n = 10 + 5
+print("10 + 5 = " + str(n))
+
+# Subtração
+sub = 10 - 5
+print("10 - 5 = " + str(sub))
+
+sub = 5 - 10
+
+print("5 - 10 = " + str(sub))
+
+# Multiplicação
+
+m = 10 * 5
+
+print("10 * 5 = " + str(m))
+
+# Divisão Real
+
+d = 5 / 2
+
+print("5 / 2 = " + str(d))
+
+# Divisão Inteira
+
+dd = 5 // 2
+
+print("5 // 2 = " + str(dd))
+
+# Resto da Divisão
+
+r = 5 % 2
+print("5 % 2 = " + str(r))
+
+# Exponenciação
+
+e = 2 ** 3
+print("2**3 = " + str(e))
+
+# Python linguagem de alto nível
+
+maximo = 2 ** 63
+print("2**63 = " + str(maximo))
+
+maximo = 2 ** 1000
+print("2**100 = " + str(maximo))
+
+# Legibilidade
+
+numero = 1_000_000
+print("1_000_000 =" + str(numero))
