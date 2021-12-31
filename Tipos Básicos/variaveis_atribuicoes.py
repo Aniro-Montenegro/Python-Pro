cecb9c1058d8bc796c47ea35538e2626488588e0
79Aniro <capablanca1@bol.com.br>
2021-12-31 14:59:26 -0300

    Variavéis e atribuição

---
+++ Tipos Básicos/variaveis_atribuicoes.py
@@ -0,0 +1,32 @@
+# Atribuição
+n = 1  # atribuição de um valor à variavel n
+
+print(n)
+
+# Variaveis validas
+
+_n = 1
+n1 = 10
+n_n = 10
+n_1 = 20
+
+# Atribuicao
+
+n = 10
+n = n + 1
+print(n)
+
+n = 2
+n += 2  # versão compacta
+
+print(n)
+
+'''
+Na atribuição a expressão a direita é avaliada, processada e atribuida a variavel a esquerda
+'''
+
+# Variaveis invalidas
+
+'''
+7n=1
+'''
