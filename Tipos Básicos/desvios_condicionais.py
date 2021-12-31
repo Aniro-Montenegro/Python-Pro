166750d7a992e7d78dcde0722965cb99e25f73c6
79Aniro <capablanca1@bol.com.br>
2021-12-31 16:30:01 -0300

    Desvios condicionais

---
+++ Tipos Básicos/desvios_condicionais.py
@@ -0,0 +1,10 @@
+#  Desvios condicionais
+
+idade = 18
+
+if idade < 18:
+    print("Menor de idade")
+elif 18 >= idade < 65:
+    print("Idade adulta")
+else:
+    print("Provavelmente aposentado")
