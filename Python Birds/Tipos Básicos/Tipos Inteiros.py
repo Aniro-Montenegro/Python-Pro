557dc3a076b4442ed9275336d41e1e468f584aaa
Aniro-Montenegro <aniromontenegro79@gmail.com>
2022-01-20 18:47:40 -0300

    Python Byrds- Tipos Inteiros

---
+++ Python Birds/Tipos Básicos/Tipos Inteiros.py
@@ -0,0 +1,27 @@
+n = 10
+print(type(n))
+
+soma = 10 + 20
+print(f'Soma {soma}')
+
+divisao = 5 / 2
+print(f'Divisao {divisao}')
+
+multiplicacao = 5 * 2
+print(f'Multiplicacao {multiplicacao}')
+
+subtracao = 9 - 2
+print(f'Subtracao {subtracao}')
+
+potencia = 2 ** 3
+print(f'Potencia {potencia}')
+
+resto_divisao = 5 % 2
+print(f'Resto da divisao {resto_divisao}')
+
+divisao_inteira = 5 // 2
+
+print(f'Divisao inteira {divisao_inteira}')
+
+n= 1_000_000
+print(n)
