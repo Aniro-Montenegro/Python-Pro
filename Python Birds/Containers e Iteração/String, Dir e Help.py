1d1ad2e7d026b66c861dd8c8ac9185ffaf4c5717
Aniro-Montenegro <aniromontenegro79@gmail.com>
2022-01-21 00:27:15 -0300

    String Dir e help

---
+++ Python Birds/Containers e Iteração/String, Dir e Help.py
@@ -0,0 +1,31 @@
+palavra = " Olá Mundo"
+
+print(type(palavra))
+
+palavra = 'Uso de aspas "duplas"'
+
+print(palavra)
+
+palavra = 'Uso de quebra de linhas\noutra linha'
+
+print(palavra)
+
+palavra = '''Uso de quebra de linhas sem n
+outra linha'''
+
+print(palavra)
+
+primeira_string = "primeira string "
+segunda_string = 'segunda_string'
+
+nova_string = primeira_string + segunda_string
+print(nova_string)
+
+palavra = "Ola " * 3
+print(palavra)
+
+print(dir(palavra))
+
+help("Metodo".lower)
+
+print("Metodo".lower())
