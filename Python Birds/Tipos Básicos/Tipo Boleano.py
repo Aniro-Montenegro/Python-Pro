bfee3ec8dc3cb9b1888aa52db5f41d7720c11582
Aniro-Montenegro <aniromontenegro79@gmail.com>
2022-01-21 00:03:02 -0300

    Tipos Booleanos

---
+++ Python Birds/Tipos Básicos/Tipo Boleano.py
@@ -0,0 +1,45 @@
+# True ou false
+
+
+verdadeiro = True
+
+false = False
+
+print(type(verdadeiro))
+
+print(not verdadeiro)
+
+print(verdadeiro and false)
+
+print(verdadeiro and verdadeiro)
+
+print(false and false)
+
+print(verdadeiro or verdadeiro)
+
+print(false or false)
+
+print(false or verdadeiro)
+
+print(5 > 6)
+
+print(6 > 5)
+
+print(5 < 6)
+
+print(5 <= 6)
+
+print(5 >= 6)
+
+print(5 == 6)
+
+print(5 != 6)
+
+n = 4
+print(1 < n and n > 5)
+
+print(1 < n > 5)
+
+print(1 < n or n > 5)
+
+print(1 < n > 5)
---
+++ Python Birds/Tipos Básicos/Variaveis e Atribuições.py
@@ -0,0 +1,35 @@
+# n= 1  variavel n recebendo o número 1
+
+n = 1
+_n = 1
+
+# Python é uma linguagem de tipagem dinâmica
+
+n = 1 + 3
+resultado = 1 + 2 + n
+
+print(resultado)
+
+n = n + 1
+# ou
+
+n += 15
+print(n)
+
+n *= 12
+print(n)
+
+n /= 3
+print(n)
+
+n **= 4
+print(n)
+
+
+n=9
+n %= 2
+print(n)
+
+n=25
+n //= 2
+print(n)
