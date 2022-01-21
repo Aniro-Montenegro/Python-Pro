cc54a894e18a5e14c92df9d6b197613a375c9231
Aniro-Montenegro <aniromontenegro79@gmail.com>
2022-01-20 23:45:12 -0300

    Float e Complex

---
+++ Python Birds/Tipos Básicos/Tipo Float.py
@@ -0,0 +1,41 @@
+n = 1.35
+
+print(type(n))
+
+soma = 10.36 + 20.2
+print(f'Soma {soma}')
+
+divisao = 5.45 / 2
+print(f'Divisao {divisao}')
+
+multiplicacao = 5.96 * 2
+print(f'Multiplicacao {multiplicacao}')
+
+subtracao = 9.6 - 2.5
+print(f'Subtracao {subtracao}')
+
+potencia = 2.5 ** 3
+print(f'Potencia {potencia}')
+
+resto_divisao = 5.63 % 2
+print(f'Resto da divisao {resto_divisao}')
+
+divisao_inteira = 5.63 // 2
+
+print(f'Divisao inteira {divisao_inteira}')
+
+n = 1_000_000.589
+print(n)
+
+n = 15.75
+
+i = int(n)
+print(type(i))
+print(i)
+
+i = 10
+n = float(i)
+print(type(n))
+
+n = 1j
+print(type(1j))
