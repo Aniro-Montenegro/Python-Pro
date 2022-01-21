ecebb25567c0a6358202daa335cc6a816d24a1eb
Aniro-Montenegro <aniromontenegro79@gmail.com>
2022-01-21 11:44:15 -0300

    Tuplas e ID

---
+++ Python Birds/Containers e Iteração/Tupla e Id.py
@@ -0,0 +1,21 @@
+tpl = (1, 2)
+print(type(tpl))
+
+tpl = tuple(range(10))
+
+print(tpl)
+
+tpl = (7)
+print(type(tpl))
+
+tpl = (7,)
+print(type(tpl))
+
+registro = ('Aniro', 42)
+
+nome, idade = registro
+print(nome)
+print(idade)
+
+print(id(tpl))
+
