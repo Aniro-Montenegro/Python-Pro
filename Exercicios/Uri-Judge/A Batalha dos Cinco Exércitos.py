4d7fe8893c667e7d8e478a2fae0366ae3c446bc4
Aniro-Montenegro <aniromontenegro79@gmail.com>
2022-02-07 10:42:47 -0300

    Uri Judge- A Batalha dos Cinco Exércitos

---
+++ Exercicios/Uri-Judge/A Batalha dos Cinco Exércitos.py
@@ -0,0 +1,30 @@
+"""
+SUBMISSÃO # 26172524
+PROBLEMA:
+3147 - A Batalha dos Cinco Exércitos
+RESPOSTA:
+Accepted
+LINGUAGEM:
+Python 3.4 (Python 3.4.3) [+1s]
+TEMPO:
+0.014s
+TAMANHO:
+236 Bytes
+MEMÓRIA:
+-
+SUBMISSÃO:
+07/02/2022 10:39:32
+
+RANKING    SUBMISSÃO     USUÁRIO	        LINGUAGEM        TEMPO          DATA DA SUBMISSÃO
+94	       26172524	     Aniro Montenegro	Python 3	     0.014	       07/02/2022 13:39:32
+"""
+
+
+numeros = input()
+lista = numeros.split(" ")
+a = int(lista[0]) + int(lista[1]) + int(lista[2]) + int(lista[5])
+b = int(lista[3]) + int(lista[4])
+if a > b:
+    print('Middle-earth is safe.')
+else:
+    print('Sauron has returned.')
