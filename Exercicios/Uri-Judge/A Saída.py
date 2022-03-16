a5494c09ada0e2911e9790680ea704626845f3f8
Aniro-Montenegro <aniromontenegro79@gmail.com>
2022-03-16 00:57:02 -0300

    Uri-Judge- A saida

---
+++ Exercicios/Uri-Judge/A Saída.py
@@ -0,0 +1,19 @@
+"""
+SUBMISSÃO # 26747288
+PROBLEMA:2861 - A Saída
+RESPOSTA:Accepted
+LINGUAGEM:Python 3.8 (Python 3.8.2) [+1s]
+TEMPO:0.139s
+TAMANHO:76 Bytes
+MEMÓRIA:-
+SUBMISSÃO:16/03/2022 00:54:48
+778	26747288	Aniro Montenegro	Python 3.8	0.139	16/03/2022 03:54:48
+
+2862	Aniro Montenegro	FATEC-SJC	793,11	↥47
+"""
+
+n=int(input())
+
+for _ in range(n):
+    palavra=input()
+    print('gzuz')
---
+++ Exercicios/Uri-Judge/ESTRUTURA DE DECISÃO/Aviões de Papel.py
@@ -0,0 +1,61 @@
+"""
+SUBMISSÃO # 26747083
+PROBLEMA:2339 - Aviões de Papel
+RESPOSTA:Accepted
+LINGUAGEM:Python 3.8 (Python 3.8.2) [+1s]
+TEMPO:0.143s
+TAMANHO:111 Bytes
+MEMÓRIA:-
+SUBMISSÃO:16/03/2022 00:27:01
+
+706	26747083	Aniro Montenegro	Python 3.8	0.143	16/03/2022 03:27:01
+"""
+
+# dados=input().split()
+#
+# if (int(dados[0])*int(dados[2]))>int(dados[1]):
+#     print('N')
+# else:
+#     print('S')
+
+
+# dados=list(map(lambda x: int(x), input().split()))
+#
+# if (int(dados[0])*int(dados[2]))>int(dados[1]):
+#     print('N')
+# else:
+#     print('S')
+
+"""
+SUBMISSÃO # 26747182
+PROBLEMA:2339 - Aviões de Papel
+RESPOSTA:Accepted
+LINGUAGEM:Python 3.8 (Python 3.8.2) [+1s]
+TEMPO:0.115s
+TAMANHO:140 Bytes
+MEMÓRIA:-
+SUBMISSÃO:16/03/2022 00:39:08
+
+689	26747182	Aniro Montenegro	Python 3.8	0.115	16/03/2022 03:27:01
+"""
+
+dados=list(map(lambda x: int(x), input().split()))
+
+if (dados[0]*dados[2])>dados[1]:
+    print('N')
+else:
+    print('S')
+
+
+"""
+SUBMISSÃO # 26747199
+PROBLEMA:2339 - Aviões de Papel
+RESPOSTA:Accepted
+LINGUAGEM:Python 3.8 (Python 3.8.2) [+1s]
+TEMPO:0.158s
+TAMANHO:125 Bytes
+MEMÓRIA:-
+SUBMISSÃO:16/03/2022 00:42:39
+
+2875	Aniro Montenegro	FATEC-SJC	791,46	↥34	
+"""
