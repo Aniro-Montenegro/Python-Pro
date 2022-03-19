d355fc7afd972a6b180d53111a777e4c1a192d79
Aniro-Montenegro <aniromontenegro79@gmail.com>
2022-03-19 12:03:39 -0300

    Uri-Judge- Entrada e Saída CPF

---
+++ Exercicios/Uri-Judge/LISTAS/Entrada e Saída CPF.py
@@ -0,0 +1,23 @@
+"""
+SUBMISSÃO # 26823446
+PROBLEMA:2763 - Entrada e Saída CPF
+RESPOSTA:Accepted
+LINGUAGEM:Python 3.8 (Python 3.8.2) [+1s]
+TEMPO:0.064s
+TAMANHO:124 Bytes
+MEMÓRIA:-
+SUBMISSÃO:19/03/2022 12:01:18
+865	26823446	Aniro Montenegro	Python 3.8	0.064	19/03/2022 15:01:18
+
+2855	Aniro Montenegro	FATEC-SJC	795,32	↥14
+
+"""
+
+
+lista=input().split(".")
+lista2=lista[2].split("-")
+print(lista[0])
+print(lista[1])
+print(lista2[0])
+print(lista2[1])
+
